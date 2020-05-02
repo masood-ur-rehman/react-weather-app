00af758b58c02ca1c876db720aef324d3f802cd6
Masood <masoodrehman39@gmail.com>
2020-05-02 18:58:10 +0500

    Temp list functionality

--- src/index.js
+++ src/index.js
@@ -1,14 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+
+import App from './components/Weather';
+import Search from './App';
+
+function Home() {
+    return (
+        <Router>
+            <div>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/search">Search</Link>
+                    </li>
+                </ul>
+                <Switch>
+                    <Route exact path="/">
+                        <App />
+                    </Route>
+                    <Route path="/search">
+                        <Search />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
+}
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Home/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
